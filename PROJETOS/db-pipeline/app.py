99551825e5051785c2658a8bee3464201d4549b3
unknown <caroll.zambelli@gmail.com>
2024-08-21 20:27:20 -0300

    correcoes no prj db-pipe

--- PROJETOS/db-pipeline/app.py
+++ PROJETOS/db-pipeline/app.py
@@ -6,39 +6,59 @@ from assets.utils import read_metadado, padroniza_str
 import logging
 import datetime
 
-
-logging.basicConfig(filename='flights_pipe.txt', level=logging.INFO)
+logging.basicConfig(filename='flights_pipe_log.txt', level=logging.INFO)
 logger = logging.getLogger()
 load_dotenv()
 
-# Função de leitura dos dados
 def read_data(data_path):
+    '''
+    Função de leitura dos dados
+    INPUT: Path do arquivo da base de dados
+    OUTPUT: Pandas DataFrame
+    '''
     df = pd.read_csv(data_path,index_col=0)
-    df["data_voo"] = pd.to_datetime(df[['year', 'month', 'day']]) 
     return df
 
-# Função de validação de nulos
 def null_check(df, null_tolerance):
+    '''
+    Função de validação de nulos
+    INPUT: Pandas DataFrame, dicionário de colunas como chave e critério de nulo como valores
+    OUTPUT: Pandas DataFrame
+    '''
     for col in null_tolerance.keys():
         if  len(df.loc[df[col].isnull()])/len(df)> null_tolerance[col]:
-            logger.warning(f'Muitos NULOS: {col} ;{datetime.datetime.now()}')
+            logger.error(
+                f" {col} possui mais nulos do que o esperado; {datetime.datetime.now()} ")
 
-# Função para excluir de nulos
 def null_exclude(df, cols_chaves):
+    '''
+    Função de exclusao das observações nulas
+    INPUT: Pandas DataFrame, lista de colunas que são chaves
+    OUTPUT: Pandas DataFrame com as observações nulas excluidas
+    '''
     tmp = df.copy()
     for col in cols_chaves:
         tmp_df = tmp.loc[~df[col].isna()]
         tmp = tmp_df.copy()
+    return tmp_df
 
-# Função de seleção e rename
-def select_rename(df,cols_originais, cols_renamed):
+def select_rename(df, cols_originais, cols_renamed):
+    '''
+    Função de validação de nulos
+    INPUT: Pandas DataFrame, lista dos nomes das colunas e lista dos novos nomes 
+    OUTPUT: Pandas DataFrame com novos nomes
+    '''
     df_work = df.loc[:, cols_originais].copy()
     columns_map = dict(zip(cols_originais,cols_renamed))
     df_work.rename(columns=columns_map, inplace = True)
     return df_work
 
-# Função de tipagem
 def convert_data_type(df, tipos_map):
+    '''
+    Função de validação de nulos
+    INPUT: Pandas DataFrame, dicionário de colunas como chave e seus tipos como valores
+    OUTPUT: Pandas DataFrame com novos nomes
+    '''
     data =df.copy()
     for col in tipos_map.keys():
         tipo = tipos_map[col]
@@ -48,24 +68,32 @@ def convert_data_type(df, tipos_map):
             data[col] = data[col].astype(float)
         elif tipo == "datetime":
             data[col] = pd.to_datetime(data[col])
+    return data
 
-# Função para tratar strings
+
 def string_std(df, std_str):
+    '''
+    Função de validação de nulos
+    INPUT: Pandas DataFrame, lista das colunas que devem receber a padronização de strings
+    OUTPUT: Pandas DataFrame com as colunas padronizadas
+    '''
     df_work = df.copy()
     for col in std_str:
         new_col = f'{col}_formatted'
         df_work[new_col] = df_work.loc[:,col].apply(lambda x: padroniza_str(x))
     return df_work
 
-# Função para tratamento de horas
-# Função de validação de chaves
-# Novos campos
-# Salvar no banco de dados
+# Criar função para tratamento de horas
+# Criar função de validação de chaves
+# Criar função para novos campos
+# Função para para salvar no banco de dados
+# exemplo: df.to_sql('mytable', con=conn, if_exists='replace')
 
 
 if __name__ == "__main__":
     logger.info(f'Inicio da execução ; {datetime.datetime.now()}')
-    cols_originais, cols_renamed, tipos_originais, tipos_formatted, cols_chaves, null_tolerance, std_str  = read_metadado(os.getenv('META_PATH'))
-    df= read_data(os.getenv('DATA_PATH'))
-    print(cols_originais, cols_renamed, tipos_originais, tipos_formatted, cols_chaves, null_tolerance, std_str)
-    print(df.head())
+    metadados  = read_metadado(os.getenv('META_PATH'))
+    df = read_data(os.getenv('DATA_PATH'))
+    # função1
+    # logger(tratamento 1 realizado)
+    logger.info(f'Fim da execução ; {datetime.datetime.now()}')
--- PROJETOS/db-pipeline/assets/utils.py
+++ PROJETOS/db-pipeline/assets/utils.py
@@ -3,37 +3,31 @@ import re
 
 def read_metadado(meta_path):
     meta = pd.read_excel(meta_path)
-    cols_originais = list(meta["cols_originais"])
-    cols_renamed = list(meta["cols_renamed"])
-    tipos_originais = dict(zip(cols_originais,list(meta["tipo_original"])))
-    tipos_formatted = dict(zip(cols_renamed,list(meta["tipo_formatted"])))
-    cols_chaves = list(meta.loc[meta["key"] == 1]["cols_originais"])
-    null_tolerance = dict(zip(cols_originais,list(meta["raw_null_tolerance"])))
-    std_str = list(meta.loc[meta["std_str"] == 1]["cols_renamed"])
-    return cols_originais, cols_renamed, tipos_originais, tipos_formatted, cols_chaves, null_tolerance, std_str
+    metadados = {
+        "cols_originais" : list(meta["cols_originais"]), 
+        "cols_renamed" : list(meta["cols_renamed"]),
+        "tipos_originais" : dict(zip(list(meta["cols_originais"]),list(meta["tipo_original"]))),
+        "tipos_formatted" : dict(zip(list(meta["cols_renamed"]),list(meta["tipo_formatted"]))),
+        "cols_chaves" : list(meta.loc[meta["key"] == 1]["cols_originais"]),
+        "null_tolerance" : dict(zip(list(meta["cols_originais"]), list(meta["raw_null_tolerance"]))),
+        "std_str" : list(meta.loc[meta["std_str"] == 1]["cols_renamed"])
+        }
+    return metadados
 
 def padroniza_str(obs):
     return re.sub('[^A-Za-z0-9]+', '', obs.lower())
 
-def corrige_hora(hr_str):
-     if len(hr_str) == 1:
-          return '00:'+ '0' + hr_str
-     elif len(hr_str) == 2:
-         if int(hr_str) <= 12:
-            return '0' + hr_str[0] + ':' + hr_str[1]+'0'
-         else:
-            return  "00:"+hr_str
-     elif len(hr_str) == 3:
-         return "0" + hr_str[0] + ':' + hr_str[1:]
-     elif len(hr_str) == 4:
-         if hr_str == "2400":
-             return "00:00"
-         else:
-             return hr_str[0:2] + ':' + hr_str[2:]
-     else:
-        return "ERROR"
+
+def corrige_hora(hr_str, dct_hora = {1:"000?",2:"00?",3:"0?",4:"?"}):
+    if hr_str == "2400":
+        return "00:00"
+    elif (len(hr_str) == 2) & (int(hr_str) <= 12):
+        return f"0{hr_str[0]}:{hr_str[1]}0"
+    else:
+        hora = dct_hora[len(hr_str)].replace("?", hr_str)
+        return f"{hora[:2]}:{hora[2:]}"
 
 
 #meta_path  = "C://Users//Carolina//OneDrive//Github//learn-python//PROJETOS//db-pipeline//assets//work_metadado_flights.xlsx"
-#cols_originais, cols_renamed, cols_tipos, cols_chaves, null_tolerance, std_str = read_metadado(meta_path)
-#print(cols_originais, cols_renamed, cols_tipos, cols_chaves, null_tolerance)
+#cols_originais, cols_renamed, tipos_originais, tipos_formatted, cols_chaves, null_tolerance, std_str = read_metadado(meta_path)
+#print(cols_originais, cols_renamed, tipos_originais, tipos_formatted, cols_chaves, null_tolerance, std_str)
